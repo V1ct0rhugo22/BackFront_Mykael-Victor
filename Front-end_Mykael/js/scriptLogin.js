2ded86c2dd741be867d63e5c412e830c8f506b1e
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-14 16:08:28 -0300

    Atualização Login

--- Front-end_Mykael/js/scriptLogin.js
+++ Front-end_Mykael/js/scriptLogin.js
@@ -1,44 +1,21 @@
-const URL = 'http:localhost:8080/professor';
+const URL = 'http://localhost:8080/professor';
 
 async function login() {
     const email = document.querySelector('#emailLogin');
     const password = document.querySelector('#passwordLogin');
 
-    // fetch(`http:localhost:8080/professor/login`, {
-    //     method: 'POST',
-    //     headers: { 
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ email, password })
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     data.sucess ? window.location.href = 'html/principal.html' : alert(data.message);
-    // }).catch(error => {
-    //     alert("Erro ao fazer login");
-    //     console.log(error);
-    // });
-
-
-
-    const response = await fetch('/login', {
+    fetch(`${URL}/login`, {
         method: 'POST',
-        headers: {
+        headers: { 
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.sucess ? window.location.href = 'html/principal.html' : alert(data.message);
+    }).catch(error => {
+        alert("Erro ao fazer login");
+        console.log(error);
     });
-
-    if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(errorText);
-    }
-
-    const data = await response.text();
-    console.log(data); // Ex: "Login bem-sucedido! Bem-vindo, email@exemplo.com"
-    return data;
-
-
-
-    
 }
