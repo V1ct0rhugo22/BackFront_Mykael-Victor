33b2dfc560cf2cc10980a7d143a78231ac35cb4b
V1ct0rhugo22 <victorhero53@gmail.com>
2025-04-14 14:07:01 -0300

    Back_end(mudança&nomes)

---
+++ Back-end_Victor/src/main/java/com/senai/FrontBack/FrontBack_Mykael_Victor/FrontBackMykaelVictorApplication.java
@@ -0,0 +1,13 @@
+package com.senai.FrontBack.FrontBack_Mykael_Victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontBackMykaelVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontBackMykaelVictorApplication.class, args);
+	}
+
+}
---
+++ Back-end_Victor/src/test/java/com/senai/FrontBack/FrontBack_Mykael_Victor/FrontBackMykaelVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.FrontBack.FrontBack_Mykael_Victor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontBackMykaelVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
