286407a82d8f79b530804906073d7ae588080771
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-14 17:23:37 -0300

    Principal até agora

--- Front-end_Mykael/js/scriptPrincipal.js
+++ Front-end_Mykael/js/scriptPrincipal.js
@@ -1,9 +1,26 @@
-const URL = '';
+const URL = 'http://localhost:8080/turma';
 
-window.onload = event => {
+window.onload = () => {
     getProducts();
 }
 
 function getProducts() {
+    fetch(`${URL}/get`)
+        .then((response) => response.json())
+        .then((data) => formataComoTabela(data))
+        .catch((error) => console.error('Erro:', error));
+}
 
+function formataComoTabela(data) {
+  let tabelaProdutos = document.getElementById('tabela-produtos');
+  for (let i = 0; i < data.length; i++) {
+    let paraInserir =
+      '<td>' +
+      data[i].id +
+      '</td>' +
+      '<td>' +
+      data[i].nome +
+      '</td>';
+    tabelaProdutos.innerHTML += paraInserir;
+  }
 }
